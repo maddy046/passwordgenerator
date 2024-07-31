3b1222f5e9b2bcdac3859ca9a5516de7aae8beca
Mudassir Hussain Ansari <89689965+maddy046@users.noreply.github.com>
2024-07-31 16:07:05 +0530

    Uploaded Password Generate File

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('generate').addEventListener('click', function() {
+  var length = document.getElementById('length').value;
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  document.getElementById('password').textContent = retVal;
+});
